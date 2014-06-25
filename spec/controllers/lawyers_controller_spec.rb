99c77635d0a71ba82a03d0c7224e80d2781fd03b
Rocky <slateness77@yahoo.com>
2014-06-25 11:52:49 -0700

    testing complete

--- spec/controllers/lawyers_controller_spec.rb
+++ spec/controllers/lawyers_controller_spec.rb
@@ -1,11 +1,41 @@
 require 'spec_helper'
 
-describe LawyersController, type: :controller do
-    let :valid_attributes do
-        {
-            name: "James Milani",
-            
+describe LawyersController do 
+	let :valid_attributes do
+		{
+			:name => "James"
+		}
+	end
 
-        }
-    end
+	describe "GET index" do
+		before do
+			get :index
+		end
+
+		it "should render the index template" do
+			expect(response).to render_template :index
+		end
+	end
+
+	describe "GET new" do
+		before do
+			get :new
+		end
+
+
+		it "should render the new template" do
+			expect(response).to render_template :new
+		end
+	end
+
+	describe "POST create" do
+	    describe "successful create" do
+	      it "should create a lawyer in the database" do
+	        expect do
+	          post :create, lawyer: valid_attributes
+	        end
+	    end
+  	end
+	end
 end
+
